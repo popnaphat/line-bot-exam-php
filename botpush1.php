18c6dd654878ae2f989a5d8dc677277f73b609d6
popnaphat <40283665+popnaphat@users.noreply.github.com>
2018-07-03 15:27:54 +0700

    Update botpush1.php

--- botpush1.php
+++ botpush1.php
@@ -1,11 +1,18 @@
 <?php
 require "vendor/autoload.php";
+
 $access_token = 'A2vi8pE1X2HAQIM8goe1u+naF4fjT21H60PhWLovTesoSjtJrm2pFXqDZsfKHzj0d+Ly1TvYtrZQfZOI++JbMGgZuWZE+5/GWhPQpgCiVhFYq3jv89lB55itG8s7y1msC0wxu2ZeMArAr5KTuCUj2gdB04t89/1O/w1cDnyilFU=';
+
 $channelSecret = '6b22c4107de7c703fea210a87981ef3a';
 
 $pushID = 'Uaa9dae6e226dcda1fd4d2b907cde6b23';
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
+
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
